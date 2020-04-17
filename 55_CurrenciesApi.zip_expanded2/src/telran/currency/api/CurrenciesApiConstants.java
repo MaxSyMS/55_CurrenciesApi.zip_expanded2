ddb76ed000f473dd5703b4fd499fc2f57f23374f
SyMS <SyMS@10.0.0.17>
2020-04-17 21:54:54 +0300

    first commit

---
+++ 55_CurrenciesApi.zip_expanded2/src/telran/currency/api/CurrenciesApiConstants.java
@@ -0,0 +1,9 @@
+package telran.currency.api;
+
+public interface CurrenciesApiConstants {
+
+	String CURRENCIES = "/currencies";
+	String DATE_TIME = "/currencies/datetime";
+	String CONVERT = "/currencies/convert";
+	String REFRESH = "/currencies/refresh";
+}
---
+++ 55_CurrenciesApi.zip_expanded2/src/telran/currency/interfaces/ICurrencyConverter.java
@@ -0,0 +1,12 @@
+package telran.currency.interfaces;
+
+import java.util.Set;
+
+public interface ICurrencyConverter {
+	
+	String lastDateTimePresentation();
+	Set<String> getCodes();
+	double convert(String from, String to, double amount);
+	String displayRefreshPeriodTime();
+
+}
